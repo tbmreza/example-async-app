6575af1447f4a320ddb9646746bccb0771e58f3f
Reza <rezahandzalah@gmail.com>
2022-02-20 05:51:43 +0700

    unittest goto and log_types commands
    we can experiment on tokio runtime structure, now that a couple of
    commands are tested.

--- src/browse.rs
+++ src/browse.rs
@@ -2,14 +2,14 @@
 use color_eyre::Result;
 use eyre::eyre;
 use std::sync::{Arc, Mutex};
-use thirtyfour::error::{WebDriverError, WebDriverResult};
+// use thirtyfour::error::{WebDriverError, WebDriverResult};
 use thirtyfour::prelude::*;
-use thirtyfour::LogType; // if cargo points to my fork
+// use thirtyfour::LogType; // if cargo points to my fork
 use tokio;
 use tokio::fs::File;
 use tokio::fs::OpenOptions;
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
-use tokio::time::{sleep, Duration};
+// use tokio::time::{sleep, Duration};
 
 pub type Urlbar = Arc<Mutex<String>>;
 
@@ -102,6 +102,59 @@ pub async fn get_log(url: &str) -> Result<()> {
 //     Ok(())
 // }
 
+/// Driver to be shared among methods.
+pub async fn new_driver(port: u16) -> Result<()> {
+    let url = format!("http://localhost:{}", port);
+    let mut caps = DesiredCapabilities::chrome();
+    caps.add_chrome_arg("--headless")?;
+
+    let driver = WebDriver::new(&url, &caps).await?;
+
+    driver.quit().await?;
+    Ok(())
+}
+
+#[test]
+fn test_new_driver() {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    rt.block_on(async {
+        if let Err(e) = new_driver(4444).await {
+            println!("{}", e);
+        }
+    });
+}
+
+#[test]
+fn test_blocking_log_types() {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    rt.block_on(async {
+        if let Err(e) = log_types().await {
+            println!("{}", e);
+        }
+    });
+}
+
+#[test]
+fn test_blocking_goto() {
+    let urlbar = Arc::new(Mutex::new(String::new()));
+    let url = "https://wikipedia.org";
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    rt.block_on(async {
+        if let Err(e) = goto(urlbar.clone(), url).await {
+            println!("{}", e);
+        }
+    });
+}
+
 /// Make driver if none is already running and query available log types.
 pub async fn log_types() -> Result<()> {
     let mut caps = DesiredCapabilities::chrome();
@@ -110,7 +163,12 @@ pub async fn log_types() -> Result<()> {
     let driver = WebDriver::new("http://localhost:4444", &caps).await?;
 
     match driver.log_types().await {
-        Ok(log_types) => println!("{:?}", log_types),
+        Ok(log_types) => {
+            let expected = ["browser".to_string(), "driver".to_string()].to_vec();
+            assert_eq!(log_types, expected);
+
+            println!("{:?}", log_types)
+        }
         Err(e) => println!("{:?}", e),
     }
 
@@ -124,25 +182,27 @@ pub async fn goto(urlbar: Urlbar, url: &str) -> Result<()> {
 
     let mut caps = DesiredCapabilities::chrome();
     caps.add_chrome_arg("--headless")?;
-    // let mut caps = DesiredCapabilities::firefox();
-    // caps.add_firefox_arg("--headless")?;
 
     let driver = WebDriver::new("http://localhost:4444", &caps).await?;
 
-    // instantiate driver in separate async function?
+    match driver.get(url).await {
+        Ok(_) => {
+            let source = driver.page_source().await?;
 
-    driver.get(url).await?;
-    let source = driver.page_source().await?;
+            assert_eq!("Wikipedia", driver.title().await?);
 
-    let mut file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .truncate(true)
-        .create(true)
-        .open("page.txt")
-        .await?;
+            let mut file = OpenOptions::new()
+                .read(true)
+                .write(true)
+                .truncate(true)
+                .create(true)
+                .open("page.txt")
+                .await?;
 
-    file.write_all(source.as_bytes()).await?;
+            file.write_all(source.as_bytes()).await?;
+        }
+        Err(e) => println!("{:?}", e),
+    }
 
     driver.quit().await?;
     Ok(())
